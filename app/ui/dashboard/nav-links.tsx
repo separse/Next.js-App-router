57faa0d0bf0c3c4107c97554d06686653fa23957
separse <sepehr.roshanpour@tabit-gmbh.de>
2026-07-27 13:26:20 +0200

    add 'use client'

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import {DocumentDuplicateIcon, HomeIcon, UserGroupIcon,} from '@heroicons/react/24/outline';
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from 'next/navigation';
 import clsx from "clsx";
 
 // Map of links to display in the side navigation.
