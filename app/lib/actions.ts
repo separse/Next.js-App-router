ad8caf939cc4a7f8494a7f92d7639628c075f2dc
separse <sepehr.roshanpour@tabit-gmbh.de>
2026-07-29 09:51:35 +0200

    edit and delete invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,7 @@ import postgres from 'postgres';
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -15,6 +14,8 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -26,11 +27,37 @@ export async function createInvoice(formData: FormData) {
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId},
+            amount      = ${amountInCents},
+            status      = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE
+              FROM invoices
+              WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
